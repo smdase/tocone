cd097567fea0bc06838d476aec50e9f455844cdf
smdase <99349067+smdase@users.noreply.github.com>
2022-02-10 14:17:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Gửi Người Tôi Yêu",
+    introTitle: 'Nguyễn Văn A',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^ Ấn Vô Đây Đi ^^',
+    title: 'Anh đã thích em ngay từ lúc ban đầu hai ta gặp nhau rùi',
+    desc: ' Chắc ông trời đã đánh cho anh một tia sét tình yêu ngay lúc đó ',
+    title: ' Vậy cho anh hỏi anh có thể trở thành người yêu của em được không?',
+    btnYes: 'Chốt Luôn Thôi Anh <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới có hơn 7 tỉ người mà sao em lại chọn anh?',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu anh không cần lý do đâu <3333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Thế Bắt Đầu Từ Bây Giờ Chúng Ta Yêu Nhau Nha<3333.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100005641248310' }
